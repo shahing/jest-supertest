c3cc37761a428fa0c7a55031a597bcca00a382a7
shahing <arrestshahin@gmail.com>
2023-02-05 23:17:08 +0000

    2nd commit

--- jest-api/specs/test.js
+++ jest-api/specs/test.js
@@ -3,7 +3,7 @@ const superagent = require("superagent");
 //require("superagent-retry")(superagent);
 
 describe("Test the Users API", () => {
-  test("It Should respond with the User details", () => {
+  test("It Should respond with 200", () => {
     request("https://httpbin.org")
       .get("/get")
       .set("Content-Type", "application/json")
